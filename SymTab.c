0aa5b844fef75d39bb437374865c6a0162c523c0
trim.cameron <trim.cameron@uwlax.edu>
2017-09-25 23:38:08 -0500

    done

--- SymTab.c
+++ SymTab.c
@@ -5,68 +5,249 @@
 
 #include "SymTab.h"
 
+//done
 struct SymTab *
 CreateSymTab(int size, char * scopeName, struct SymTab * parentTable) {
+  struct SymTab * table=malloc(sizeof(struct SymTab));
+  table->size=size;
+  if(scopeName==NULL){
+    table->scopeName=NULL;
+  }else{
+    table->scopeName=strdup(scopeName);
+  }
+  table->parent=parentTable;
+  table->contents=malloc(sizeof(struct SymEntry *)*size);
+  return table;
 }
 
-struct SymTab *
-DestroySymTab(struct SymTab *aTable) {
-}
-
+//done
 int
 HashName(int size, const char *name) {
+  if(size==0) return -1;
+  int i;
+  int value=0;
+  for(i=0;i<strlen(name);i++){
+    value=value+name[i];
+  }
+  return value%size;
 }
 
+//done
 struct SymEntry *
 FindHashedName(struct SymTab *aTable, int hashValue, const char *name) {
+  struct SymEntry * entry=malloc(sizeof(struct SymEntry *));
+  if(aTable->contents[hashValue]==NULL){
+    return NULL;
+  }
+  entry=aTable->contents[hashValue];
+  int match = 0;
+  if(entry==NULL)return NULL;
+  if(strcmp(entry->name,name)==0) {
+    match=1;
+  }
+  while(match==0&&entry->next!=NULL){
+    entry=entry->next;
+    if(strcmp(entry->name,name)==0){
+      match=1;
+    }
+  }
+  if(match==0){
+    return entry->next;
+  }
+  return entry;
 }
 
+//done
 struct SymEntry *
 LookupName(struct SymTab *aTable, const char * name) {
+  if(aTable==NULL||name==NULL)return NULL;
+  struct SymEntry * entry=FindHashedName(aTable,HashName(aTable->size,name),name);
+  if(entry==NULL){
+    return NULL;
+  }
+  return entry;
 }
 
+//done
 struct SymEntry *
 EnterName(struct SymTab *aTable, const char *name) {
+  struct SymEntry * newEntry=malloc(sizeof(struct SymEntry *));
+  struct SymEntry * result=malloc(sizeof(struct SymEntry *));
+  result = LookupName(aTable,name);
+  if(result==NULL){
+    if(name==NULL){
+      newEntry->name=NULL;
+    }else{
+      newEntry->name=strdup(name);
+    }
+    newEntry->next=aTable->contents[HashName(aTable->size,name)];
+    newEntry->table=aTable;
+    result=newEntry;
+    aTable->contents[HashName(aTable->size,name)]=result;
+  }
+  return result;
 }
 
+//done
 int
 GetAttrKind(struct SymEntry *anEntry) {
+  return anEntry->attrKind;
 }
 
+//done
 void
 SetAttr(struct SymEntry *anEntry, int kind, void *attributes) {
+  if(kind!=NULL){
+    anEntry->attrKind = kind;
+  }else{
+    anEntry->attrKind=-1;
+  }
+  if(attributes!=NULL){
+    anEntry->attributes = attributes;
+  }else{
+    anEntry->attributes = NULL;
+  }
 }
 
+//done
 void *
 GetAttr(struct SymEntry *anEntry) {
+  if(anEntry==NULL) return NULL;
+  if(anEntry->attributes==NULL) return NULL;
+  return anEntry->attributes;
+  //return NULL;
 }
 
+//done
 const char *
 GetName(struct SymEntry *anEntry) {
+  if(anEntry==NULL||anEntry->name==NULL)return NULL;
+  return anEntry->name;
 }
 
+//done
 struct SymTab *
 GetTable(struct SymEntry *anEntry) {
+  if(anEntry==NULL||anEntry->table==NULL)return NULL;
+  return anEntry->table;
 }
 
+//done
 const char *
 GetScopeName(struct SymTab *aTable) {
+  if(aTable==NULL)return NULL;
+  if(aTable->scopeName==NULL)return NULL;
+  return aTable->scopeName;
 }
 
+//done
 char *
 GetScope(struct SymTab *aTable) {
+  char * scope = " ";
+  char * result=" ";
+  if(aTable==NULL) return NULL;
+  if(GetScopeName(aTable)==NULL)return NULL;
+  scope=strdup(GetScopeName(aTable));
+  if(scope==NULL)return NULL;
+  while(aTable->parent!=NULL){
+    aTable=aTable->parent;
+    result=malloc(sizeof(scope)+strlen(GetScopeName(aTable))+1);
+    strcpy(result,GetScopeName(aTable));
+    strcat(result," ");
+    strcat(result,scope);
+    scope=strdup(result);
+  }
+  return scope;
 }
 
+//done
 struct SymTab *
 GetParentTable(struct SymTab *aTable) {
+  return aTable->parent;
 }
 
+//done
+struct SymTab *
+DestroySymTab(struct SymTab *aTable) {
+  struct SymTab * table;
+  if(aTable==NULL)return NULL;
+  if(aTable->parent!=NULL){
+    table=malloc(sizeof(struct SymTab));
+    table=aTable->parent;
+  }else{
+    table=NULL;
+  }
+  aTable->size=0;
+  if(aTable->scopeName!=NULL) free(aTable->scopeName);
+  free(aTable->contents);
+  free(aTable);
+  return table;
+}
+
+//done
 void
-DoForEntries(struct SymTab *aTable, bool includeParentTable,
-             void (*entryFunc)(struct SymEntry * entry, int cnt, void * args),
-             int startCnt, void * withArgs) {
+DoForEntries(struct SymTab *aTable, bool includeParentTable,void (*entryFunc)(struct SymEntry * entry, int cnt, void * args), int startCnt, void * withArgs) {
+  //printf("Going through entries \n");
+  struct SymEntry * next=malloc(sizeof(struct SymEntry *));
+  int i;
+  struct SymEntry * head;
+  while(aTable!=NULL){
+    for(i=0;i<aTable->size;i++){
+      head=aTable->contents[i];
+      while(head!=NULL){
+        next=head->next;
+        entryFunc(head,startCnt,withArgs);
+        startCnt+=1;
+        head=next;
+      }
+
+    //printf("%i\n",i );
+
+      if(includeParentTable){
+      aTable=aTable->parent;
+        if(aTable==NULL)return;
+    }else {return;}
+    }
+      return;
+  }
+}
+//done
+void *
+freeEntry(struct SymEntry * current , int cnt , void * args){
+  //printf("Freeing entry \n");
+  if(current->table!=NULL)free(current->table);
+  if(current->name!=NULL)free(current->name);
+  if(&current->attrKind!=NULL)free(&current->attrKind);
+  if(current->attributes!=NULL)free(current->attributes);
+  if(current->next!=NULL)free(current->next);
+  free(current);
 }
 
+//done
 struct Stats *
 Statistics(struct SymTab *aTable) {
+  int avgL=0;
+  int maxL=0;
+  int minL=INT_MAX;
+  int eCnt=0;
+  struct Stats *sta=malloc(sizeof(struct Stats*));
+  int i;
+  struct SymEntry * head;
+  for(i=0;i<aTable->size;i++){
+    int chainLen=0;
+    head=aTable->contents[i];
+    while(head!=NULL){
+      chainLen++;
+      head=head->next;
+    }
+    if(chainLen<minL) minL=chainLen;
+    if(chainLen>maxL) maxL=chainLen;
+    eCnt=eCnt+chainLen;
+  }
+  avgL=eCnt/aTable->size;
+  sta->minLen=minL;
+  sta->maxLen=maxL;
+  sta->avgLen=avgL;
+  sta->entryCnt=eCnt;
+  return sta;
 }
